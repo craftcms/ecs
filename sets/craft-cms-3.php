8547a813e62a7f76019cda04f7761b09e19fa9c2
brandonkelly <brandon@pixelandtonic.com>
2022-03-08 08:53:16 -0800

    Removed unused import

--- sets/craft-cms-3.php
+++ sets/craft-cms-3.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use craft\ecs\fixers\ElseIfFixer;
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer;
 use PhpCsFixer\Fixer\FunctionNotation\FunctionDeclarationFixer;
